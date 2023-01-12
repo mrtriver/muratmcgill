dbf362d3e8cf8e4320fefa2ed2336d7321c9c67c
muratirmak <mrtriver@gmail.com>
2023-01-12 13:41:00 -0500

    basic installation express and app initiated

---
+++ ycit-031/lecture-6-pg-express/server.js
@@ -0,0 +1,11 @@
+const { application } = require('express');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3333, () => {
+    console.log("Server is running on port 3333");
+})
